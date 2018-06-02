e1f2665912d7563dc786b9aa8b4e4c253275d48c
SIWIITHI <siwiphorn.thi@gmail.com>
2018-06-03 01:12:50 +0700

    Calculator
    Calculator by me

--- buffet-bill-calculation/src/App.js
+++ buffet-bill-calculation/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BillDisplay from './billCalculate/billDisplay';
+import Calculator from './common/calculator';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+       <BillDisplay />
+       <Calculator />
       </div>
     );
   }
